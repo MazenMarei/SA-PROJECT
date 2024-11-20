a0ddcdd6e76a2173fdc4db41e84f46c5d887e24f
mohamed hossny <mohamed.tryndmare@gmail.com>
2024-11-20 22:13:11 +0200

    popular-books

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!');
+// tabs
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all the tab buttons
+    const tabs = document.querySelectorAll('.tabs .tab');
+
+  // Add event listeners to each tab
+    tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+      // Get the target tab content ID from the clicked tab's data attribute
+        const targetContentId = tab.getAttribute('data-tab-target');
+      // Remove the 'active' class from all tabs and tab contents
+        tabs.forEach(t => t.classList.remove('active'));
+        document.querySelectorAll('.tab-content > div').forEach(content => content.classList.remove('active'));
+      // Add the 'active' class to the clicked tab and the corresponding tab content
+        tab.classList.add('active');
+        document.querySelector(targetContentId).classList.add('active');
+    });
+});
+});
